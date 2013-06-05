59e316d0460768111dc213f4912b9bd6d4f1c6b8
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-06-04 22:32:30 -0700

    Make drawing commands functions instead of macros so they can be stubbed when testing with midje 'provided' clauses.

--- src/fui/drawing.clj
+++ src/fui/drawing.clj
@@ -3,13 +3,13 @@
 
 (def color-rgb (memoize (fn [r g b] (new Color r g b))))
 
-(defmacro fill-rect [bounds color]
+(defn fill-rect [bounds color]
   {:shape :rect, :bounds bounds, :color color})
 
-(defmacro fill-oval [bounds color]
+(defn fill-oval [bounds color]
   {:shape :oval, :bounds bounds, :color color})
 
-(defmacro draw-text
+(defn draw-text
   ([text left-x baseline-y] (draw-text text left-x baseline-y (color-rgb 0 0 0) "Lucida Grande" 13))
   ([text left-x baseline-y color font font-size] 
     {:shape :text, :text text 
